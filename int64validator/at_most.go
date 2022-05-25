ba6785a0367b2f3af46b0f57c6313ebaab838a1e
Brian Flad <bflad417@gmail.com>
2022-05-25 12:54:14 -0400

    Move shared type validation functions to separate source file and add unit testing
    Reference: https://github.com/hashicorp/terraform-plugin-framework-validators/pull/23#discussion_r881645421

--- int64validator/at_most.go
+++ int64validator/at_most.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/validatordiag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var _ tfsdk.AttributeValidator = atMostValidator{}
@@ -56,23 +55,4 @@ func AtMost(max int64) tfsdk.AttributeValidator {
 	return atMostValidator{
 		max: max,
 	}
-}
-
-// validateInt ensures that the request contains an Int64 value.
-func validateInt(ctx context.Context, request tfsdk.ValidateAttributeRequest, response *tfsdk.ValidateAttributeResponse) (int64, bool) {
-	var n types.Int64
-
-	diags := tfsdk.ValueAs(ctx, request.AttributeConfig, &n)
-
-	if diags.HasError() {
-		response.Diagnostics = append(response.Diagnostics, diags...)
-
-		return 0, false
-	}
-
-	if n.Unknown || n.Null {
-		return 0, false
-	}
-
-	return n.Value, true
 }
--- stringvalidator/length_at_most.go
+++ stringvalidator/length_at_most.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/validatordiag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var _ tfsdk.AttributeValidator = lengthAtMostValidator{}
@@ -60,23 +59,4 @@ func LengthAtMost(maxLength int) tfsdk.AttributeValidator {
 	return lengthAtMostValidator{
 		maxLength: maxLength,
 	}
-}
-
-// validateString ensures that the request contains a String value.
-func validateString(ctx context.Context, request tfsdk.ValidateAttributeRequest, response *tfsdk.ValidateAttributeResponse) (string, bool) {
-	var s types.String
-
-	diags := tfsdk.ValueAs(ctx, request.AttributeConfig, &s)
-
-	if diags.HasError() {
-		response.Diagnostics = append(response.Diagnostics, diags...)
-
-		return "", false
-	}
-
-	if s.Unknown || s.Null {
-		return "", false
-	}
-
-	return s.Value, true
 }
