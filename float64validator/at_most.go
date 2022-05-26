7c732e820bf3c72d33fd9afcdcbcf30c46ff58e0
Brian Flad <bflad417@gmail.com>
2022-05-26 16:58:57 -0500

    Move shared type validation functions to separate source file and add unit testing (#26)
    Reference: https://github.com/hashicorp/terraform-plugin-framework-validators/pull/23#discussion_r881645421

--- float64validator/at_most.go
+++ float64validator/at_most.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/validatordiag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var _ tfsdk.AttributeValidator = atMostValidator{}
@@ -56,23 +55,4 @@ func AtMost(max float64) tfsdk.AttributeValidator {
 	return atMostValidator{
 		max: max,
 	}
-}
-
-// validateFloat ensures that the request contains a Float64 value.
-func validateFloat(ctx context.Context, request tfsdk.ValidateAttributeRequest, response *tfsdk.ValidateAttributeResponse) (float64, bool) {
-	var n types.Float64
-
-	diags := tfsdk.ValueAs(ctx, request.AttributeConfig, &n)
-
-	if diags.HasError() {
-		response.Diagnostics = append(response.Diagnostics, diags...)
-
-		return 0, false
-	}
-
-	if n.Unknown || n.Null {
-		return 0, false
-	}
-
-	return n.Value, true
 }
