1d01994886e0dfc8908c432e2442ad63142b267e
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-06-21 17:03:07 +0100

    Adding List validation for SizeAtLeast, SizeAtMost and SizeBetween (#4)

---
+++ listvalidator/size_at_most.go
@@ -0,0 +1,51 @@
+package listvalidator
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/validatordiag"
+)
+
+var _ tfsdk.AttributeValidator = sizeAtMostValidator{}
+
+// sizeAtMostValidator validates that list contains at most max elements.
+type sizeAtMostValidator struct {
+	max int
+}
+
+// Description describes the validation in plain text formatting.
+func (v sizeAtMostValidator) Description(ctx context.Context) string {
+	return fmt.Sprintf("list must contain at most %d elements", v.max)
+}
+
+// MarkdownDescription describes the validation in Markdown formatting.
+func (v sizeAtMostValidator) MarkdownDescription(ctx context.Context) string {
+	return v.Description(ctx)
+}
+
+// Validate performs the validation.
+func (v sizeAtMostValidator) Validate(ctx context.Context, req tfsdk.ValidateAttributeRequest, resp *tfsdk.ValidateAttributeResponse) {
+	elems, ok := validateList(ctx, req, resp)
+	if !ok {
+		return
+	}
+
+	if len(elems) > v.max {
+		resp.Diagnostics.Append(validatordiag.AttributeValueDiagnostic(
+			req.AttributePath,
+			v.Description(ctx),
+			fmt.Sprintf("%d", len(elems)),
+		))
+
+		return
+	}
+}
+
+func SizeAtMost(max int) tfsdk.AttributeValidator {
+	return sizeAtMostValidator{
+		max: max,
+	}
+}
