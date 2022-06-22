3f2da3729c7a926bf9f5c9b9c450ac98dee51b97
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-06-22 18:23:33 +0100

    Remove "public" mention of `primitivevalidator` package

--- internal/primitivevalidator/doc.go
+++
@@ -1,4 +0,0 @@
-// Package primitivevalidator provides validators for attributes
-// of "primitive" type (i.e. `types.String`, `types.Number`, `types.Int64`,
-// `types.Float64` and `types.Bool`).
-package primitivevalidator
