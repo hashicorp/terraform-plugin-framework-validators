789bc5a38761daef6fcfeaa4f227f6b1ee79d752
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-06-22 17:49:06 +0100

    Use explicit `.Type()` interrogation instead of reflection to validate type

--- float64validator/type_validation.go
+++ float64validator/type_validation.go
@@ -3,25 +3,28 @@ package float64validator
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/helpers/validatordiag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 // validateFloat ensures that the request contains a Float64 value.
 func validateFloat(ctx context.Context, request tfsdk.ValidateAttributeRequest, response *tfsdk.ValidateAttributeResponse) (float64, bool) {
-	var n types.Float64
-
-	diags := tfsdk.ValueAs(ctx, request.AttributeConfig, &n)
-
-	if diags.HasError() {
-		response.Diagnostics = append(response.Diagnostics, diags...)
-
-		return 0, false
+	t := request.AttributeConfig.Type(ctx)
+	if t != types.Float64Type {
+		response.Diagnostics.Append(validatordiag.InvalidTypeDiagnostic(
+			request.AttributePath,
+			"Expected value of type float64",
+			t.String(),
+		))
+		return 0.0, false
 	}
 
-	if n.Unknown || n.Null {
-		return 0, false
+	f := request.AttributeConfig.(types.Float64)
+
+	if f.Unknown || f.Null {
+		return 0.0, false
 	}
 
-	return n.Value, true
+	return f.Value, true
 }
