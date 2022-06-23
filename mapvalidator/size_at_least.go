d3ecda1b316fbc107374162448bb7f1894275d2a
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-06-23 12:59:23 +0100

    Adding Map Validation for SizeAtLeast, SizeAtMost and SizeBetween (#39)
    * Adding Map validation for SizeAtLeast (#6)
    
    * Adding Map validation for SizeAtMost (#6)
    
    * Adding Map validation for SizeBetween (#6)
    
    * Update following code review (#6)
    
    * Rename .changelog file to match PR number (#6)

---
+++ mapvalidator/size_at_least.go
@@ -0,0 +1,58 @@
+package mapvalidator
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/validatordiag"
+)
+
+var _ tfsdk.AttributeValidator = sizeAtLeastValidator{}
+
+// sizeAtLeastValidator validates that map contains at least min elements.
+type sizeAtLeastValidator struct {
+	min int
+}
+
+// Description describes the validation in plain text formatting.
+func (v sizeAtLeastValidator) Description(ctx context.Context) string {
+	return fmt.Sprintf("map must contain at least %d elements", v.min)
+}
+
+// MarkdownDescription describes the validation in Markdown formatting.
+func (v sizeAtLeastValidator) MarkdownDescription(ctx context.Context) string {
+	return v.Description(ctx)
+}
+
+// Validate performs the validation.
+func (v sizeAtLeastValidator) Validate(ctx context.Context, req tfsdk.ValidateAttributeRequest, resp *tfsdk.ValidateAttributeResponse) {
+	elems, ok := validateMap(ctx, req, resp)
+	if !ok {
+		return
+	}
+
+	if len(elems) < v.min {
+		resp.Diagnostics.Append(validatordiag.AttributeValueDiagnostic(
+			req.AttributePath,
+			v.Description(ctx),
+			fmt.Sprintf("%d", len(elems)),
+		))
+
+		return
+	}
+}
+
+// SizeAtLeast returns an AttributeValidator which ensures that any configured
+// attribute value:
+//
+//     - Is a Map.
+//     - Contains at least min elements.
+//
+// Null (unconfigured) and unknown (known after apply) values are skipped.
+func SizeAtLeast(min int) tfsdk.AttributeValidator {
+	return sizeAtLeastValidator{
+		min: min,
+	}
+}
