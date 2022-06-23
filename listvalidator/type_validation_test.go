4af42b152815f98e2ed488282edfaf3f9214b31d
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-06-23 12:52:53 +0100

    Adding List element validation for ValuesAre (#37)
    * Adding List element validation for ValuesAre (#11)
    
    * Renaming test file (#11)
    
    * Rename test (#11)
    
    * Fix tests (#11)
    
    * Updating CHANGELOG (#11)
    
    * Adding doc.go for listvalidator pkg (#11)
    
    * Adding docs and completing all validations before returning (#11)
    
    * Rename .changelog file to match PR number and remove updates to CHANGELOG.md (#11)

---
+++ listvalidator/type_validation_test.go
@@ -0,0 +1,82 @@
+package listvalidator
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+func TestValidateList(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		request           tfsdk.ValidateAttributeRequest
+		expectedListElems []attr.Value
+		expectedOk        bool
+	}{
+		"invalid-type": {
+			request: tfsdk.ValidateAttributeRequest{
+				AttributeConfig: types.Bool{Value: true},
+				AttributePath:   tftypes.NewAttributePath().WithAttributeName("test"),
+			},
+			expectedListElems: nil,
+			expectedOk:        false,
+		},
+		"list-null": {
+			request: tfsdk.ValidateAttributeRequest{
+				AttributeConfig: types.List{Null: true},
+				AttributePath:   tftypes.NewAttributePath().WithAttributeName("test"),
+			},
+			expectedListElems: nil,
+			expectedOk:        false,
+		},
+		"list-unknown": {
+			request: tfsdk.ValidateAttributeRequest{
+				AttributeConfig: types.List{Unknown: true},
+				AttributePath:   tftypes.NewAttributePath().WithAttributeName("test"),
+			},
+			expectedListElems: nil,
+			expectedOk:        false,
+		},
+		"list-value": {
+			request: tfsdk.ValidateAttributeRequest{
+				AttributeConfig: types.List{
+					ElemType: types.StringType,
+					Elems: []attr.Value{
+						types.String{Value: "first"},
+						types.String{Value: "second"},
+					},
+				},
+				AttributePath: tftypes.NewAttributePath().WithAttributeName("test"),
+			},
+			expectedListElems: []attr.Value{
+				types.String{Value: "first"},
+				types.String{Value: "second"},
+			},
+			expectedOk: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			gotListElems, gotOk := validateList(context.Background(), testCase.request, &tfsdk.ValidateAttributeResponse{})
+
+			if diff := cmp.Diff(gotListElems, testCase.expectedListElems); diff != "" {
+				t.Errorf("unexpected float64 difference: %s", diff)
+			}
+
+			if diff := cmp.Diff(gotOk, testCase.expectedOk); diff != "" {
+				t.Errorf("unexpected ok difference: %s", diff)
+			}
+		})
+	}
+}
