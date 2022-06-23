1a3ea196ea26340845f0309dd31d4f6d275dccb6
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-06-23 12:54:47 +0100

    Adding Map validation for KeysAre and ValuesAre (#38)
    * Adding Map validation for KeysAre and ValuesAre (#13)
    
    * Updates following code review (#13)
    
    * Update CHANGELOG.md (#13)
    
    * Rename .changelog file to match PR number and remove updates to CHANGELOG.md (#13)
    
    * Updating docs to clarify validation of Map keys (#13)

---
+++ mapvalidator/keys_are.go
@@ -0,0 +1,64 @@
+package mapvalidator
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var _ tfsdk.AttributeValidator = keysAreValidator{}
+
+// keysAreValidator validates that each map key validates against each of the value validators.
+type keysAreValidator struct {
+	keyValidators []tfsdk.AttributeValidator
+}
+
+// Description describes the validation in plain text formatting.
+func (v keysAreValidator) Description(ctx context.Context) string {
+	var descriptions []string
+	for _, validator := range v.keyValidators {
+		descriptions = append(descriptions, validator.Description(ctx))
+	}
+
+	return fmt.Sprintf("key must satisfy all validations: %s", strings.Join(descriptions, " + "))
+}
+
+// MarkdownDescription describes the validation in Markdown formatting.
+func (v keysAreValidator) MarkdownDescription(ctx context.Context) string {
+	return v.Description(ctx)
+}
+
+// Validate performs the validation.
+// Note that the AttributePath specified in the ValidateAttributeRequest refers to the value in the Map with key `k`,
+// whereas the AttributeConfig refers to the key itself (i.e., `k`). This is intentional as the validation being
+// performed is for the keys of the Map.
+func (v keysAreValidator) Validate(ctx context.Context, req tfsdk.ValidateAttributeRequest, resp *tfsdk.ValidateAttributeResponse) {
+	elems, ok := validateMap(ctx, req, resp)
+	if !ok {
+		return
+	}
+
+	for k := range elems {
+		request := tfsdk.ValidateAttributeRequest{
+			AttributePath:   req.AttributePath.WithElementKeyString(k),
+			AttributeConfig: types.String{Value: k},
+			Config:          req.Config,
+		}
+
+		for _, validator := range v.keyValidators {
+			validator.Validate(ctx, request, resp)
+			if resp.Diagnostics.HasError() {
+				return
+			}
+		}
+	}
+}
+
+func KeysAre(keyValidators ...tfsdk.AttributeValidator) tfsdk.AttributeValidator {
+	return keysAreValidator{
+		keyValidators: keyValidators,
+	}
+}
---
+++ mapvalidator/type_validation_test.go
@@ -0,0 +1,82 @@
+package mapvalidator
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+func TestValidateMap(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		request     tfsdk.ValidateAttributeRequest
+		expectedMap map[string]attr.Value
+		expectedOk  bool
+	}{
+		"invalid-type": {
+			request: tfsdk.ValidateAttributeRequest{
+				AttributeConfig: types.Bool{Value: true},
+				AttributePath:   tftypes.NewAttributePath().WithAttributeName("test"),
+			},
+			expectedMap: nil,
+			expectedOk:  false,
+		},
+		"map-null": {
+			request: tfsdk.ValidateAttributeRequest{
+				AttributeConfig: types.Map{Null: true},
+				AttributePath:   tftypes.NewAttributePath().WithAttributeName("test"),
+			},
+			expectedMap: nil,
+			expectedOk:  false,
+		},
+		"map-unknown": {
+			request: tfsdk.ValidateAttributeRequest{
+				AttributeConfig: types.Map{Unknown: true},
+				AttributePath:   tftypes.NewAttributePath().WithAttributeName("test"),
+			},
+			expectedMap: nil,
+			expectedOk:  false,
+		},
+		"map-value": {
+			request: tfsdk.ValidateAttributeRequest{
+				AttributeConfig: types.Map{
+					ElemType: types.StringType,
+					Elems: map[string]attr.Value{
+						"one": types.String{Value: "first"},
+						"two": types.String{Value: "second"},
+					},
+				},
+				AttributePath: tftypes.NewAttributePath().WithAttributeName("test"),
+			},
+			expectedMap: map[string]attr.Value{
+				"one": types.String{Value: "first"},
+				"two": types.String{Value: "second"},
+			},
+			expectedOk: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			gotMapElems, gotOk := validateMap(context.Background(), testCase.request, &tfsdk.ValidateAttributeResponse{})
+
+			if diff := cmp.Diff(gotMapElems, testCase.expectedMap); diff != "" {
+				t.Errorf("unexpected map difference: %s", diff)
+			}
+
+			if diff := cmp.Diff(gotOk, testCase.expectedOk); diff != "" {
+				t.Errorf("unexpected ok difference: %s", diff)
+			}
+		})
+	}
+}
