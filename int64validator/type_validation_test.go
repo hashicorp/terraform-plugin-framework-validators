9c2061dfb74ee7bdee5d5bba6d314ea649a235e1
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-07-19 15:27:23 +0100

    Rely on 'tfsdk.ValueAs' to do type validation

--- int64validator/type_validation_test.go
+++ int64validator/type_validation_test.go
@@ -14,9 +14,11 @@ func TestValidateInt(t *testing.T) {
 	t.Parallel()
 
 	testCases := map[string]struct {
-		request       tfsdk.ValidateAttributeRequest
-		expectedInt64 int64
-		expectedOk    bool
+		request             tfsdk.ValidateAttributeRequest
+		expectedInt64       int64
+		expectedOk          bool
+		expectedDiagSummary string
+		expectedDiagDetail  string
 	}{
 		"invalid-type": {
 			request: tfsdk.ValidateAttributeRequest{
@@ -24,8 +26,10 @@ func TestValidateInt(t *testing.T) {
 				AttributePath:           path.Root("test"),
 				AttributePathExpression: path.MatchRoot("test"),
 			},
-			expectedInt64: 0.0,
-			expectedOk:    false,
+			expectedInt64:       0.0,
+			expectedOk:          false,
+			expectedDiagSummary: "Value Conversion Error",
+			expectedDiagDetail:  "An unexpected error was encountered trying to convert into a Terraform value. This is always an error in the provider. Please report the following to the provider developer:\n\nCannot use attr.Value types.Int64, only types.Bool is supported because types.primitive is the type in the schema",
 		},
 		"int64-null": {
 			request: tfsdk.ValidateAttributeRequest{
@@ -33,8 +37,10 @@ func TestValidateInt(t *testing.T) {
 				AttributePath:           path.Root("test"),
 				AttributePathExpression: path.MatchRoot("test"),
 			},
-			expectedInt64: 0.0,
-			expectedOk:    false,
+			expectedInt64:       0.0,
+			expectedOk:          false,
+			expectedDiagSummary: "",
+			expectedDiagDetail:  "",
 		},
 		"int64-value": {
 			request: tfsdk.ValidateAttributeRequest{
@@ -51,8 +57,10 @@ func TestValidateInt(t *testing.T) {
 				AttributePath:           path.Root("test"),
 				AttributePathExpression: path.MatchRoot("test"),
 			},
-			expectedInt64: 0.0,
-			expectedOk:    false,
+			expectedInt64:       0.0,
+			expectedOk:          false,
+			expectedDiagSummary: "",
+			expectedDiagDetail:  "",
 		},
 	}
 
@@ -62,10 +70,25 @@ func TestValidateInt(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			gotInt64, gotOk := validateInt(context.Background(), testCase.request, &tfsdk.ValidateAttributeResponse{})
+			res := tfsdk.ValidateAttributeResponse{}
+			gotInt64, gotOk := validateInt(context.Background(), testCase.request, &res)
+
+			if res.Diagnostics.HasError() {
+				if res.Diagnostics.ErrorsCount() != 1 {
+					t.Errorf("expected an error but found none")
+				} else {
+					if diff := cmp.Diff(res.Diagnostics[0].Summary(), testCase.expectedDiagSummary); diff != "" {
+						t.Errorf("unexpected diagnostic summary difference: %s", diff)
+					}
+
+					if diff := cmp.Diff(res.Diagnostics[0].Detail(), testCase.expectedDiagDetail); diff != "" {
+						t.Errorf("unexpected diagnostic summary difference: %s", diff)
+					}
+				}
+			}
 
 			if diff := cmp.Diff(gotInt64, testCase.expectedInt64); diff != "" {
-				t.Errorf("unexpected float64 difference: %s", diff)
+				t.Errorf("unexpected int64 difference: %s", diff)
 			}
 
 			if diff := cmp.Diff(gotOk, testCase.expectedOk); diff != "" {
