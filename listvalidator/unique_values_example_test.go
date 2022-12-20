8e6bfb172a6f747b460076df60536b085fdacbf6
Brian Flad <bflad417@gmail.com>
2022-12-20 05:49:34 -0800

    listvalidator: Added UniqueValues validator (#88)
    Reference: https://github.com/hashicorp/terraform-plugin-framework-validators/issues/67

---
+++ listvalidator/unique_values_example_test.go
@@ -0,0 +1,24 @@
+package listvalidator_test
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func ExampleUniqueValues() {
+	// Used within a Schema method of a DataSource, Provider, or Resource
+	_ = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"example_attr": schema.ListAttribute{
+				ElementType: types.StringType,
+				Required:    true,
+				Validators: []validator.List{
+					// Validate this list must contain only unique values.
+					listvalidator.UniqueValues(),
+				},
+			},
+		},
+	}
+}
