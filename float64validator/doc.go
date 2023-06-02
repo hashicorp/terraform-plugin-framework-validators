49da373e5a024ada656d047c917858bfcb8a23a4
Austin Valle <austinvalle@gmail.com>
2023-06-02 11:21:37 -0400

    [COMPLIANCE] Add copyright and license headers (#132)
    * add copywrite changes
    
    * [COMPLIANCE] Add copywrite
    
    * dependabot and corrected go version

--- float64validator/doc.go
+++ float64validator/doc.go
@@ -1,2 +1,5 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 // Package float64validator provides validators for types.Float64 attributes.
 package float64validator
--- numbervalidator/doc.go
+++ numbervalidator/doc.go
@@ -1,2 +1,5 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 // Package numbervalidator provides validators for types.Number attributes.
 package numbervalidator
