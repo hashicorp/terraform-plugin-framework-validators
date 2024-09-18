4f1d1e9322e5abc321d9e68a05f904863164c6ba
Jared Baker <jared.baker@hashicorp.com>
2024-09-18 16:39:44 -0400

    boolvalidator: add Equals validator
    This validator can be used in cases where non-null boolean value should be exactly `true` or exactly `false`.
    
    ```console
    % go test -count=1 ./boolvalidator/...
    ok      github.com/hashicorp/terraform-plugin-framework-validators/boolvalidator        0.251s
    ```

---
+++ boolvalidator/equals.go
@@ -0,0 +1,51 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package boolvalidator
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/helpers/validatordiag"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var _ validator.Bool = equalsValidator{}
+
+type equalsValidator struct {
+	value types.Bool
+}
+
+func (v equalsValidator) Description(ctx context.Context) string {
+	return fmt.Sprintf("Value must be %q", v.value)
+}
+
+func (v equalsValidator) MarkdownDescription(ctx context.Context) string {
+	return v.Description(ctx)
+}
+
+func (v equalsValidator) ValidateBool(ctx context.Context, req validator.BoolRequest, resp *validator.BoolResponse) {
+	if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
+		return
+	}
+
+	configValue := req.ConfigValue
+
+	if !configValue.Equal(v.value) {
+		resp.Diagnostics.Append(validatordiag.InvalidAttributeValueMatchDiagnostic(
+			req.Path,
+			v.Description(ctx),
+			configValue.String(),
+		))
+	}
+}
+
+// Equals checks that the Bool held in the attribute matches the
+// given `value`
+func Equals(value bool) validator.Bool {
+	return equalsValidator{
+		value: types.BoolValue(value),
+	}
+}
