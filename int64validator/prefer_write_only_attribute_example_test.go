f48686118e5e8bda8d37fc0fc0149c68836b03d7
Selena Goods <github@simplebox.anonaddy.com>
2024-10-07 17:32:35 -0400

    Implement `PreferWriteOnlyAttribute` schema validator and attribute validator implementations

---
+++ int64validator/prefer_write_only_attribute_example_test.go
@@ -0,0 +1,34 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package int64validator_test
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
+)
+
+func ExamplePreferWriteOnlyAttribute() {
+	// Used within a Schema method of a Resource
+	_ = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"example_attr": schema.Int64Attribute{
+				Optional: true,
+				Validators: []validator.Int64{
+					// Throws a warning diagnostic encouraging practitioners to use
+					// write_only_attr if example_attr has a value.
+					int64validator.PreferWriteOnlyAttribute(
+						path.MatchRoot("write_only_attr"),
+					),
+				},
+			},
+			"write_only_attr": schema.Int64Attribute{
+				WriteOnly: true,
+				Optional:  true,
+			},
+		},
+	}
+}
